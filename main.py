0aa01329c5985eadd0853f1e671be3d6fbd3a433
ApaTia Python <108146296+ApaT1aPY@users.noreply.github.com>
2024-04-23 23:58:58 +0300

    Фикс

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from vkbottle.bot import Bot, Message
 from config import token
-from list import *
+
 
 bot = Bot(token)
 bot.labeler.vbml_ignore_case = True
 
-@bot.on.chat_message(privet)
+
+@bot.on.chat_message(text=["привет", "hello", "hi", "ку", "qq", "q", "хай", "дарова", "здравствуй", "доброго времени суток", "вечер в хату", "здравствуйте", "здарова", "пиривирет", "приветик", "приветули", "алоха"])
 async def message_handler(message: Message):
     user = await bot.api.users.get(message.from_id)
     if user[0].first_name == "Артём":
@@ -22,5 +23,6 @@ async def message_handler(message: Message):
                await message.reply(f"Привет, {user[0].first_name}❤") 
     else:
         await message.reply(f"Привет, {user[0].first_name}❤")
+
 
 bot.run_forever()
